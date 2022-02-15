6bb1fe24f0498894b7685af06721937229af39b1
unknown <vrnranjan22@gmail.com>
2022-02-15 11:03:13 +0530

    basic setup of html and css is done

---
+++ sandbox.ts
@@ -0,0 +1,21 @@
+const circle = (diameter: number) => diameter * Math.PI
+
+console.log(circle(3.4))
+
+let arr = ["hello", "world", 25]
+
+arr.push(89)
+
+arr[0] = 75
+console.log(arr)
+
+let ninja = {
+    fname: "varun"
+}
+ninja["lname"] = "ffe"
+ninja["age"] = 30
+ninja["skilss"] = ["fighting", "killing"]
+
+console.log(ninja)
+
+
